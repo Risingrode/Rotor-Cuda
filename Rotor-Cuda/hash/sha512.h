2e7b99d4e16e276034be24d03f82439946dbfaa6
fcw <3185087246@qq.com>
2026-04-02 03:52:21 +0000

    基础优化

--- Rotor-Cuda/hash/sha512.h
+++ Rotor-Cuda/hash/sha512.h
@@ -18,7 +18,7 @@
 #ifndef SHA512_H
 #define SHA512_H
 #include <string>
-
+#include <cstdint>
 void sha512(unsigned char *input, int length, unsigned char *digest);
 void pbkdf2_hmac_sha512(uint8_t *out, size_t outlen, const uint8_t *passwd, size_t passlen, const uint8_t *salt, size_t saltlen, uint64_t iter);
 void hmac_sha512(unsigned char *key, int key_length, unsigned char *message, int message_length, unsigned char *digest);
