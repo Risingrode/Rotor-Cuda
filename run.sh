91e980e0f367a6a61f99daa44eb959d21f0ce4c6
fcw <3185087246@qq.com>
2026-04-03 02:30:16 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 export PREFIX='xxx'
 export ADDR='xxxx'
 export SETS_REAL='01,AB,12,BC,23,CD,34,DC,43,ED,54,FE01,6578,0F12,76,10,87,2134,98AB,3245,A9BC,4356,BACD'
